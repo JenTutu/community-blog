a727d0aa282ecc1c03dfad335c6588d718f72717
Jennifer <JennieO23@gmail.com>
2019-06-15 10:46:07 -0500

    add registration errors + loading state
    redirect to Home on successful registration
    use localStorage for authenticated user

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+
+library.add(faSpinner)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.config.productionTip = false
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,17 +5,27 @@
         <div class="card-body">
           <h3 class="text-center my-4">Signup</h3>
           <div class="form-group">
-            <input v-model="name" type="text" placeholder="Name" class="form-control">
+            <input :class="{'is-invalid': errors.name, 'is-valid': !errors.name && submitted }" v-model="name" type="text" placeholder="Name" class="form-control">
+            <div class="errors" v-if="errors.name">
+              <small class="text-danger" :key="error" v-for="error in errors.name">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group">
-            <input v-model="email" type="text" placeholder="Email" class="form-control">
+            <input :class="{'is-invalid': errors.email, 'is-valid': !errors.email && submitted }" v-model="email" type="text" placeholder="Email" class="form-control">
+            <div class="errors" v-if="errors.email">
+              <small class="text-danger" :key="error" v-for="error in errors.email">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group">
-            <input v-model="password" type="password" placeholder="Password" class="form-control">
+            <input :class="{ 'is-invalid': errors.password, 'is-valid': !errors.password && submitted }" v-model="password" type="password" placeholder="Password" class="form-control">
+            <div class="errors" v-if="errors.password">
+              <small class="text-danger" :key="error" v-for="error in errors.password">{{ error }}</small>
+            </div>
           </div>
           <div class="form-group text-center">
-            <button @click="registerUser" class="btn btn-success form-control">
-              Signup
+            <button @click="registerUser" :disabled="loading" class="btn btn-success form-control">
+              <font-awesome-icon :icon="['fas', 'spinner']" size="1x" v-if="loading" />
+              {{loading ? '' : 'Signup'}}
             </button>
           </div>
         </div>
@@ -32,21 +42,31 @@ export default {
     return {
       name: '',
       email: '',
-      password: ''
+      password: '',
+      errors: {},
+      submitted: false,
+      loading: false
     };
   },
   methods: {
     registerUser() {
+      this.loading = true
       axios.post('https://react-blog-api.bahdcasts.com/api/auth/register', {
         name: this.name,
         email: this.email,
         password: this.password
       }).then(response => {
+        this.loading = false
+        this.submitted = true;
         const { data } = response.data; // destructuring: this gets the data property from the object on the right.
         localStorage.setItem('auth', JSON.stringify(data));
         this.$root.auth = data; // have to also set here, since localStorage check is only run 1x, when the page loads
+
+        this.$router.push('home');
       }).catch(({response}) => { // shorthand for getting the response property of the error object.
-        console.log(response)
+        this.loading = false;
+        this.submitted = true;
+        this.errors = response.data;
       });
     }
   }
